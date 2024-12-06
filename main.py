27b866f83b32b62fd255922591f1c8990faa309c
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-06 10:24:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
+#Запрашивает у пользователя количество строк. (должно быть проверка)
 spisok = []
 j = 0
-while True:
-    a = input("vvedite kolvo slov ")
-    if a.isdigit() and int(a) > 0:#если все цифры и int > 0
-        kolvo = int(a)
+a = input("введите колво слов: ")
+if a.isdigit() and int(a) > 0:#если все цифры и int > 0
+    kolvo = int(a)
         
-        for i in range(kolvo):#i по длине kolvo
-            b = input("vvedite slovo ")
-            spisok.append(b)
-        print(spisok)
-        word = set(spisok)#множество spisok
-        for i in word:
-            spisok_count = spisok.count(i)#проверяет сколько раз i вошло в функцию
-            print(i, "повторилась ", spisok_count)
-        break
+    for i in range(kolvo):#i по длине kolvo
+        b = input("введите слово ")
+        spisok.append(b)
+    print(spisok)
+    word = set(spisok)#множество spisok
+    for i in word:
+        spisok_count = spisok.count(i)#проверяет сколько раз i вошло в функцию
+        print(i, "повторилась ", spisok_count)
+
     
-    else:
-        print("!!!!!!!!!!!! vvedite normalno !!!!!!!!!!!!")
-    break
+else:
+    print("!!!!!!!!!!!! Введите нормально!!!!!!!!!!!!")
+
     
